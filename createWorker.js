8c674ba7f00f51b27de3bb2e5c4095c448063ff5
chenpaz123 <chenpaz10@gmail.com>
2023-02-16 19:22:38 +0200

    idk

--- createWorker.js
+++ createWorker.js
@@ -1,9 +1,9 @@
-const resolvePaths = require('./utils/resolvePaths');
-const circularize = require('./utils/circularize');
-const createJob = require('./createJob');
-const { log } = require('./utils/log');
-const getId = require('./utils/getId');
-const { defaultOEM } = require('./constants/config');
+const resolvePaths = require("./utils/resolvePaths");
+const circularize = require("./utils/circularize");
+const createJob = require("./createJob");
+const { log } = require("./utils/log");
+const getId = require("./utils/getId");
+const { defaultOEM } = require("./constants/config");
 const {
   defaultOptions,
   spawnWorker,
@@ -11,17 +11,13 @@ const {
   onMessage,
   loadImage,
   send,
-} = require('./worker/node');
+} = require("./worker/node");
 
 let workerCounter = 0;
 
 module.exports = (_options = {}) => {
-  const id = getId('Worker', workerCounter);
-  const {
-    logger,
-    errorHandler,
-    ...options
-  } = resolvePaths({
+  const id = getId("Worker", workerCounter);
+  const { logger, errorHandler, ...options } = resolvePaths({
     ...defaultOptions,
     ..._options,
   });
@@ -39,7 +35,7 @@ module.exports = (_options = {}) => {
     rejects[action] = rej;
   };
 
-  const startJob = ({ id: jobId, action, payload }) => (
+  const startJob = ({ id: jobId, action, payload }) =>
     new Promise((resolve, reject) => {
       log(`[${id}]: Start ${jobId}, action=${action}`);
       setResolve(action, resolve);
@@ -50,123 +46,133 @@ module.exports = (_options = {}) => {
         action,
         payload,
       });
-    })
-  );
+    });
 
-  const load = (jobId) => (
-    startJob(createJob({
-      id: jobId, action: 'load', payload: { options },
-    }))
-  );
+  const load = (jobId) =>
+    startJob(
+      createJob({
+        id: jobId,
+        action: "load",
+        payload: { options },
+      })
+    );
 
-  const writeText = (path, text, jobId) => (
-    startJob(createJob({
-      id: jobId,
-      action: 'FS',
-      payload: { method: 'writeFile', args: [path, text] },
-    }))
-  );
+  const writeText = (path, text, jobId) =>
+    startJob(
+      createJob({
+        id: jobId,
+        action: "FS",
+        payload: { method: "writeFile", args: [path, text] },
+      })
+    );
 
-  const readText = (path, jobId) => (
-    startJob(createJob({
-      id: jobId,
-      action: 'FS',
-      payload: { method: 'readFile', args: [path, { encoding: 'utf8' }] },
-    }))
-  );
+  const readText = (path, jobId) =>
+    startJob(
+      createJob({
+        id: jobId,
+        action: "FS",
+        payload: { method: "readFile", args: [path, { encoding: "utf8" }] },
+      })
+    );
 
-  const removeFile = (path, jobId) => (
-    startJob(createJob({
-      id: jobId,
-      action: 'FS',
-      payload: { method: 'unlink', args: [path] },
-    }))
-  );
+  const removeFile = (path, jobId) =>
+    startJob(
+      createJob({
+        id: jobId,
+        action: "FS",
+        payload: { method: "unlink", args: [path] },
+      })
+    );
 
-  const FS = (method, args, jobId) => (
-    startJob(createJob({
-      id: jobId,
-      action: 'FS',
-      payload: { method, args },
-    }))
-  );
+  const FS = (method, args, jobId) =>
+    startJob(
+      createJob({
+        id: jobId,
+        action: "FS",
+        payload: { method, args },
+      })
+    );
 
-  const loadLanguage = (langs = 'eng', jobId) => (
-    startJob(createJob({
-      id: jobId,
-      action: 'loadLanguage',
-      payload: { langs, options },
-    }))
-  );
+  const loadLanguage = (langs = "eng", jobId) =>
+    startJob(
+      createJob({
+        id: jobId,
+        action: "loadLanguage",
+        payload: { langs, options },
+      })
+    );
 
-  const initialize = (langs = 'eng', oem = defaultOEM, jobId) => (
-    startJob(createJob({
-      id: jobId,
-      action: 'initialize',
-      payload: { langs, oem },
-    }))
-  );
+  const initialize = (langs = "eng", oem = defaultOEM, jobId) =>
+    startJob(
+      createJob({
+        id: jobId,
+        action: "initialize",
+        payload: { langs, oem },
+      })
+    );
 
-  const setParameters = (params = {}, jobId) => (
-    startJob(createJob({
-      id: jobId,
-      action: 'setParameters',
-      payload: { params },
-    }))
-  );
+  const setParameters = (params = {}, jobId) =>
+    startJob(
+      createJob({
+        id: jobId,
+        action: "setParameters",
+        payload: { params },
+      })
+    );
 
-  const recognize = async (image, opts = {}, jobId) => (
-    startJob(createJob({
-      id: jobId,
-      action: 'recognize',
-      payload: { image: await loadImage(image), options: opts },
-    }))
-  );
+  const recognize = async (image, opts = {}, jobId) =>
+    startJob(
+      createJob({
+        id: jobId,
+        action: "recognize",
+        payload: { image: await loadImage(image), options: opts },
+      })
+    );
 
-  const getPDF = (title = 'Tesseract OCR Result', textonly = false, jobId) => (
-    startJob(createJob({
-      id: jobId,
-      action: 'getPDF',
-      payload: { title, textonly },
-    }))
-  );
+  const getPDF = (title = "Tesseract OCR Result", textonly = false, jobId) =>
+    startJob(
+      createJob({
+        id: jobId,
+        action: "getPDF",
+        payload: { title, textonly },
+      })
+    );
 
-  const detect = async (image, jobId) => (
-    startJob(createJob({
-      id: jobId,
-      action: 'detect',
-      payload: { image: await loadImage(image) },
-    }))
-  );
+  const detect = async (image, jobId) =>
+    startJob(
+      createJob({
+        id: jobId,
+        action: "detect",
+        payload: { image: await loadImage(image) },
+      })
+    );
 
   const terminate = async () => {
-		if(this.worker) adapter.terminateWorker(this);
-		this.worker = null;
-		this._currentJob = null;
-		this._queue = [];
+    if (this.worker) adapter.terminateWorker(this);
+    this.worker = null;
+    this._currentJob = null;
+    this._queue = [];
     return Promise.resolve();
   };
 
-  onMessage(worker, ({
-    workerId, jobId, status, action, data,
-  }) => {
-    if (status === 'resolve') {
+  onMessage(worker, ({ workerId, jobId, status, action, data }) => {
+    if (status === "resolve") {
       log(`[${workerId}]: Complete ${jobId}`);
       let d = data;
-      if (action === 'recognize') {
+      if (action === "recognize") {
         d = circularize(data);
-      } else if (action === 'getPDF') {
+      } else if (action === "getPDF") {
         d = Array.from({ ...data, length: Object.keys(data).length });
       }
       resolves[action]({ jobId, data: d });
-    } else if (status === 'reject') {
+    } else if (status === "reject") {
       rejects[action](data);
       if (errorHandler) {
         errorHandler(data);
       } else {
         throw Error(data);
       }
-    } else if (status === 'progress') {
+    } else if (status === "progress") {
       logger({ ...data, userJobId: jobId });
     }
   });
