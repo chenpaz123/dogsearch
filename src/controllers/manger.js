9053beee954f3494e1ef25b08cd0700d2d95f5f0
chenpaz123 <chenpaz10@gmail.com>
2023-04-11 20:15:53 +0300

    updated manager functions

--- src/controllers/manger.js
+++ src/controllers/manger.js
@@ -2,9 +2,15 @@ const { db, auth } = require("../firebase/firebase");
 const { fixdatetodot } = require("../utils/fixdate");
 //const {firebase} = require("firebase")
 // make the document name as the show name and date and put no data in it
-const createShow = async (KenelClubName, date) => {
+
+const createShow = async (KenelClubName, date, desc) => {
   date = fixdatetodot(date);
   const show = await db.collection("shows").doc(`${KenelClubName} - ${date}`);
+  //set the desc as a field in the show document
+  const showdesc = await db
+    .collection("shows")
+    .doc(`${KenelClubName} - ${date}`);
+
   try {
     if ((await show.get()).exists) {
       return {
@@ -13,6 +19,7 @@ const createShow = async (KenelClubName, date) => {
       };
     }
     await show.set({});
+    await showdesc.set({ desc: desc });
     return {
       message: "show created",
       status: 200,
@@ -25,48 +32,43 @@ const createShow = async (KenelClubName, date) => {
   }
 };
 
-// create a func that gets show name and date and updates it on the database
-
-const updateshow = async (getshow, newkennelclub, newdate) => {
-  const show = await db.collection("shows").doc(`${getshow}`);
-  newdate = fixdatetodot(newdate);
-  const newshow = await db
-    .collection("shows")
-    .doc(`${newkennelclub} - ${newdate}`);
-  //replace the old show with the new one
-  try {
-    if (!(await show.get()).exists) {
-      return {
-        message: "show does not exist",
-        status: 400,
-      };
-    }
-    const data = await show.get();
-    await newshow.set(data.data());
-    await show.delete();
-    return {
-      message: "show updated",
-      status: 200,
-    };
-  } catch (error) {
-    return {
-      error: error,
-      status: 400,
-    };
-  }
+//create a func that returns all the shows as objects with the show name and date
+const getallshows = async () => {
+  //create an array to store the shows names
+  const shows = [];
+  //get all the shows from the database
+  const showsdata = await db.collection("shows").get();
+  //split the show name and date
+  showsdata.forEach((show) => {
+    const showname = show.id.split(" - ");
+    shows.push({
+      name: showname[0],
+      date: showname[1],
+      desc: show.data().desc,
+    });
+  });
+  //return the array
+  return {
+    shows: shows,
+    message: "shows returned",
+    status: 200,
+  };
 };
 
-// create a func that gets show name and date and delete it from the database
-const deleteShow = async (getshow) => {
-  const show = await db.collection("shows").doc(`${getshow}`);
+//create a func that gets show object and delte it from the database
+const deleteShow = async (show) => {
+  console.log(show);
+  console.log("in func");
+  const showname = `${show.name} - ${show.date}`;
+  const showdoc = await db.collection("shows").doc(`${showname}`);
   try {
-    if (!(await show.get()).exists) {
+    if (!(await showdoc.get()).exists) {
       return {
         message: "show does not exist",
         status: 400,
       };
     }
-    await show.delete();
+    await showdoc.delete();
     return {
       message: "show deleted",
       status: 200,
@@ -78,110 +80,42 @@ const deleteShow = async (getshow) => {
     };
   }
 };
+//create a func that gets a old show object and a new show object and updates the old show with the new one replacing the name and date and the desc
+// withouth deleting the content of the old show, just update name and date and desc
 
-const getallshows = async () => {
-  //create an array to store the shows names
-  const shows = [];
-  //get all the shows from the database
-  const showsdata = await db.collection("shows").get();
-  //loop through the shows and push the show name to the array
-  showsdata.forEach((show) => {
-    shows.push(show.id);
-  });
-  //return the array
-  return {
-    shows: shows,
-    message: "shows returned",
-    status: 200,
-  };
-};
-
-const authmanger = async (email, password) => {
+const updateshow = async (oldshow, updatedshow) => {
+  console.log(oldshow, updatedshow);
+  const oldshowname = `${oldshow.name} - ${oldshow.date}`;
+  const oldshowdoc = await db.collection("shows").doc(`${oldshowname}`);
+  updatedshow.date = fixdatetodot(updatedshow.date);
+  const newshowname = `${updatedshow.name} - ${updatedshow.date}`;
+  const newshowdoc = await db.collection("shows").doc(`${newshowname}`);
+  //update the old show with the new one
   try {
-    console.log(email, password);
-    //create the user with the email and password
-    const user = await auth.createUser({
-      email: email,
-      password: password,
+    if (!(await oldshowdoc.get()).exists) {
+      return {
+        message: "show does not exist",
+        status: 400,
+      };
+    }
+    await newshowdoc.set({
+      desc: updatedshow.desc,
     });
-    console.log(user);
-    //return the user
+    await oldshowdoc.delete();
     return {
-      user: user,
-      message: "manger created",
+      message: "show updated",
       status: 200,
     };
   } catch (error) {
     return {
-      error: error.message,
+      error: error,
       status: 400,
     };
   }
 };
-
-const loginmanger = async (email, password) => {
-  try {
-    const user = await auth.signInWithEmailAndPassword({
-      email: email,
-      password: password,
-    });
-    //return the user
-    return {
-      user: user,
-      message: "manger logged in",
-      status: 200,
-    };
-  } catch (error) {
-    return {
-      error: error.message,
-      status: 400,
-    };
-  }
-};
-
-const logoutmanger = async () => {
-  try {
-    await auth.unauth();
-    return {
-      message: "manger logged out",
-      status: 200,
-    };
-  } catch (error) {
-    return {
-      message: "manger not logged in",
-      status: 400,
-    };
-  }
-};
-
-const deleteManger = async (email, password) => {
-  try {
-    console.log(email, password);
-    const user = await auth.signInWithEmailAndPassword({
-      email: email,
-      password: password,
-    });
-    console.log(user);
-    await user.delete();
-    return {
-      message: "manger deleted",
-      status: 200,
-    };
-  } catch (error) {
-    return {
-      error: error.message,
-      status: 400,
-    };
-  }
-};
-
 module.exports = {
   createShow,
-  updateshow,
-  deleteShow,
-  authmanger,
-  loginmanger,
   getallshows,
-  logoutmanger,
-  deleteManger,
+  deleteShow,
+  updateshow,
 };
--- src/routers/manger.js
+++ src/routers/manger.js
@@ -4,62 +4,35 @@ const {
   createShow,
   updateshow,
   deleteShow,
-  authmanger,
-  loginmanger,
   getallshows,
-  deleteManger,
-  logoutmanger,
 } = require("../controllers/manger");
 
 router.post("/createShow", async (req, res) => {
   const KenelClubName = req.body.KenelClubName;
   const date = req.body.date;
-  const response = await createShow(KenelClubName, date);
+  const desc = req.body.desc;
+  const response = await createShow(KenelClubName, date, desc);
   res.send(response);
 });
 
 router.patch("/updateshow", async (req, res) => {
-  const getshow = req.body.show;
-  const newkennelclub = req.body.newkennelclub;
-  const newdate = req.body.newdate;
-  const response = await updateshow(getshow, newkennelclub, newdate);
+  oldshow = req.body.oldshow;
+  updatedshow = req.body.updatedshow;
+  console.log(oldshow, updatedshow);
+  const response = await updateshow(oldshow, updatedshow);
   res.send(response);
 });
 
 router.delete("/deleteShow", async (req, res) => {
-  const getshow = req.body.show;
+  console.log(req.body);
+  console.log("in router");
+  const getshow = req.body;
   const response = await deleteShow(getshow);
-  res.send(response);
-});
-
-router.post("/authmanger", async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const response = await authmanger(email, password);
   res.send(response);
 });
 
 router.get("/getallshows", async (req, res) => {
   const response = await getallshows();
-  res.send(response);
-});
-
-router.post("/loginmanger", async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const response = await loginmanger(email, password);
-  res.send(response);
-});
-
-router.delete("/deleteManger", async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const response = await deleteManger(email, password);
-  res.send(response);
-});
-
-router.post("/logoutmanger", async (req, res) => {
-  const response = await logoutmanger();
   res.send(response);
 });
 
